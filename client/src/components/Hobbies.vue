d30b7c7f992229f4441c0d312689dfb56d53ca65
Jonah Engelund <jonahengelund@gmail.com>
2019-01-11 22:09:58 -0700

    Styling begins!

--- client/src/components/Hobbies.vue
+++ client/src/components/Hobbies.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <h2 class="w p-3" v-for="(postArray, hobbiName) in hobbies" @click="posts = postArray">{{hobbiName}}</h2>
+      <h2 class="w p-3 hover" v-for="(postArray, hobbiName) in hobbies" @click="posts = postArray">{{hobbiName}}</h2>
       <post v-for="post in posts" :key="post._id" :postData="post"></post>
     </div>
   </div>
@@ -33,5 +33,6 @@
   .w {
     color: whitesmoke;
     width: fit-content;
+    cursor: pointer;
   }
 </style>
